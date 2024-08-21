8d27bc05fc70992801bdda3e65cffb0fd89a20bd
Your Name <rakzar041@gmail.com>
2024-08-21 21:53:03 +0300

    Тут тепер є плюс рядок.

--- src/Lesson1/Homework2.java
+++ src/Lesson1/Homework2.java
@@ -6,8 +6,9 @@ public class Homework2 {
     public static void main(String[] args) {
         System.out.println(convertCelsiusToFahrenheit(100));
     }
+
     public static double convertCelsiusToFahrenheit(double celsius) {
-        double TF = celsius * 5/9 + 32;
+        double TF = celsius * 5 / 9 + 32;
         return TF;
     }
 }
