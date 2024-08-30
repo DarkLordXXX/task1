20e019b628b867002d48bd346a4129a2cd5718ac
Your Name <rakzar041@gmail.com>
2024-08-30 21:34:21 +0300

    Тепер порядок.Поправка суму показує добре.

--- src/Lesson2/home4.java
+++ src/Lesson2/home4.java
@@ -2,11 +2,13 @@ package Lesson2;
 
 public class home4 {
     public static void main(String[] args) {
-        //2 урок 4 завданя. Напишіть програму для обчислення суми перших 10 натуральних чисел.
-        int sum = 0;
-        for (int x = 0; x < 10; x++) {
-            sum += x;
-            System.out.println("сума перших 10 числел :" + sum);
+        //2 урок 4 завдання. Напишіть програму для обчислення суми перших 10 натуральних чисел.
+        int i, num = 10, sum = 0;
+        for(i = 1; i <= num; ++i)
+        {
+            sum = sum + i;
         }
+        System.out.println("Sum of First 10 Natural Numbers is = " + sum);
     }
 }
+
