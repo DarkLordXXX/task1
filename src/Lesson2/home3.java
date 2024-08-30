63ec4d4ff7361db3944828eaf47f457df14de7a4
Your Name <rakzar041@gmail.com>
2024-08-30 18:25:06 +0300

    Тут  2 урок 3 завдання.

---
+++ src/Lesson2/home3.java
@@ -0,0 +1,17 @@
+package Lesson2;
+
+public class home3 {
+    public static void main(String[] args) {
+        //2 урок 3 завдання.Напишіть програму для порівняння двох чисел.
+        float a = 123.23f;
+        float b = 23.23f;
+        if (a > b) {
+            System.out.println("a, більше b");
+        }
+        if (b > a) {
+            System.out.println("b, більше а");
+        } else {
+            System.out.println("Тут кінець програми ...+ _ +");
+        }
+    }
+}
