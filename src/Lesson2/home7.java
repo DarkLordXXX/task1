e894e9e7dc0bb5f90c3307f2441e34b9bbb5675f
Your Name <rakzar041@gmail.com>
2024-08-30 22:12:27 +0300

    Знайшов те що треба.

--- src/Lesson2/home7.java
+++ src/Lesson2/home7.java
@@ -4,25 +4,31 @@ import java.util.Scanner;
 
 public class home7 {
     public static void main(String[] args) {
-        //7. Напишіть програму для обчислення відстані між двома точками на поверхні землі.
+        //2.7 урок. Напишіть програму для обчислення відстані між двома точками на поверхні землі.
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введіть  x1 ціле число: ");
-        int x1 = scanner.nextInt();
+        System.out.print("Введіть широту координати 1: ");
+        double lat1 = scanner.nextDouble();
+        System.out.print("Введіть довготу координати 1: ");
+        double lon1 = scanner.nextDouble();
 
-        System.out.println("Введіть  x2 ціле число: ");
-        int x2 = scanner.nextInt();
+        System.out.print("Введіть широту координати 2: ");
+        double lat2 = scanner.nextDouble();
+        System.out.print("Введіть довготу координати 2: ");
+        double lon2 = scanner.nextDouble();
 
-        System.out.println("Введіть  y1 ціле число: ");
-        int y1 = scanner.nextInt();
-
-        System.out.println("Введіть  у2 ціле число: ");
-        int y2 = scanner.nextInt();
-
-        int sum1 = x1 - x2;
-        int sum2 = y1 - y2;
-
-        System.out.println("Відстань мінь між кординатами х-сів : " + sum1);
-        System.out.println("Відстань мінь між кординатами у-гриків : " + sum2);
+        System.out.print("Відстань між цими точками дорівнює : " + distance_Between_LatLong(lat1, lon1, lat2, lon2) + " km\n");
+    }
+    // Точки будуть перетворені в радіани перед обчисленням
+    public static double distance_Between_LatLong(double lat1, double lon1, double lat2, double lon2) {
+        // Перетворення широти та довготи в радіани
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+        // Середній радіус Землі в кілометрах
+        double earthRadius = 6371.01;
+        // Обчислити відстань за формулою гаверсинуса
+        return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
     }
 }
