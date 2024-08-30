a7432fd97fb1c9c89249adb46f102b594b61e755
Your Name <rakzar041@gmail.com>
2024-08-30 18:23:28 +0300

    Тут  2 урок 1 завдання.

---
+++ src/Lesson2/home1.java
@@ -0,0 +1,16 @@
+package Lesson2;
+
+public class home1 {
+    public static void main(String[] args) {
+        //2 урок 1 завдання. Напишіть програму на Java для друку суми (додавання), множення, віднімання, ділення та
+        //залишку двох чисел.
+        int a1 = 10;
+        int a2 = 10;
+        int sum1 = a1 + a2;
+        int sum2 = a1 - a2;
+        int sum3 = a1 * a2;
+        int sum4 = a1 / a2;
+        int sum5 = a1 % a2;
+        System.out.println(sum1 + " " + sum2 + " " + sum3 + " " + sum4 + " " + sum5);
+    }
+}
