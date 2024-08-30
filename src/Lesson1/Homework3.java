578ef677d002f6046000a22c03a8312ce0904042
Your Name <rakzar041@gmail.com>
2024-08-30 17:44:53 +0300

    Поправив, тепер дабли не інти.

--- src/Lesson1/Homework3.java
+++ src/Lesson1/Homework3.java
@@ -7,9 +7,9 @@ public class Homework3 {
         // Дюйми в сантиметри, як в 4 завданні
         System.out.println("Введіть дюйми : ");
         Scanner scanner = new Scanner(System.in);
-        int inches = scanner.nextInt();
+        double inches = scanner.nextDouble();
         System.out.println(inches + " дюймів рівно");
-        double centimetres =  inches* 2.54;
-        System.out.println(centimetres+ "  сантиметрам");;
+        double centimetres = inches * 2.54;
+        System.out.println(centimetres + "  сантиметрам");
     }
 }
