426ab151de255c547d30fab4808bd63d71647c15
Your Name <rakzar041@gmail.com>
2024-08-30 18:25:43 +0300

    Тут  2 урок 4 завдання.

---
+++ src/Lesson2/home4.java
@@ -0,0 +1,12 @@
+package Lesson2;
+
+public class home4 {
+    public static void main(String[] args) {
+        //2 урок 4 завданя. Напишіть програму для обчислення суми перших 10 натуральних чисел.
+        int sum = 0;
+        for (int x = 0; x < 10; x++) {
+            sum += x;
+            System.out.println("сума перших 10 числел :" + sum);
+        }
+    }
+}
