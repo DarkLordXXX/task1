2ef08bda250fd3f4f58c2918c5a334acbf1cd925
Your Name <rakzar041@gmail.com>
2024-08-30 19:35:36 +0300

    Поправив назви змінних, тепер вивводить в різних рядках.

--- src/Lesson2/home1.java
+++ src/Lesson2/home1.java
@@ -2,15 +2,19 @@ package Lesson2;
 
 public class home1 {
     public static void main(String[] args) {
-        //2 урок 1 завдання. Напишіть програму на Java для друку суми (додавання), множення, віднімання, ділення та
+        //2 урок 1 завдання. Напишіть програму на Java для друку суми додавання, множення, віднімання, ділення та
         //залишку двох чисел.
         int a1 = 10;
         int a2 = 10;
-        int sum1 = a1 + a2;
-        int sum2 = a1 - a2;
-        int sum3 = a1 * a2;
-        int sum4 = a1 / a2;
-        int sum5 = a1 % a2;
-        System.out.println(sum1 + " " + sum2 + " " + sum3 + " " + sum4 + " " + sum5);
+        int addition = a1 + a2;
+        int subtraction = a1 - a2;
+        int multiplication = a1 * a2;
+        int division = a1 / a2;
+        int remainder = a1 % a2;
+        System.out.println("Сума додавання " + addition );
+        System.out.println("Результат віднімання " + subtraction );
+        System.out.println("Сума множення " + multiplication);
+        System.out.println("Сума ділення " + division);
+        System.out.println("Сума залишку " + remainder);
     }
 }
