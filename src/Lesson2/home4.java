eabb1fdb07079ecc08e4acbbc5243e01cf24256d
Your Name <rakzar041@gmail.com>
2024-08-31 11:27:33 +0300

    2.4 стер num змінив на 10 і поправив формат.

--- src/Lesson2/home4.java
+++ src/Lesson2/home4.java
@@ -3,9 +3,8 @@ package Lesson2;
 public class home4 {
     public static void main(String[] args) {
         //2 урок 4 завдання. Напишіть програму для обчислення суми перших 10 натуральних чисел.
-        int i, num = 10, sum = 0;
-        for(i = 1; i <= num; ++i)
-        {
+        int i, sum = 0;
+        for (i = 1; i <= 10; ++i) {
             sum = sum + i;
         }
         System.out.println("Sum of First 10 Natural Numbers is = " + sum);
