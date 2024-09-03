5c55084d4fdeeea8e11c3cd345f2196aeaff3512
Your Name <rakzar041@gmail.com>
2024-09-03 16:01:36 +0300

    2.5 тепер те що треба 123= 1+2+3= 6

--- src/Lesson2/home5.java
+++ src/Lesson2/home5.java
@@ -6,13 +6,16 @@ public class home5 {
     public static void main(String[] args) {
         //2 урок 5 завдання. Напишіть програму для обчислення суми цифр цілого числа
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть  1 ціле число: ");
-        int number1 = scanner.nextInt();
-        System.out.print("Введіть  2 ціле число: ");
-        int number2 = scanner.nextInt();
-        int sum = number1 + number2;
-        System.out.println("Сума цифр: " + sum);
+        int number, digit, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введіть число: ");
+        number = sc.nextInt();
+        while (number > 0) {
+            digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+        }
+        System.out.println("Сума : " + sum);
     }
 }
 
