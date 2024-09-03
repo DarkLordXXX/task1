ad18663e9aab7b5cc60747f3564892e8d64f9ef6
Your Name <rakzar041@gmail.com>
2024-09-03 17:39:31 +0300

    5.2 Друге завдання практики.

---
+++ src/PracticeLesson5/task2.java
@@ -0,0 +1,19 @@
+package PracticeLesson5;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.sum;
+
+public class task2 {
+    public static void main(String[] args) {
+        //5.2. Написати програму Java яка виводить в консоль передані їй аргументи розділені знаком “ ” як один
+        //рядок.
+        int x, y, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введи 1 число: ");
+        x = sc.nextInt();
+        System.out.print("Введи 2 число: ");
+        y = sc.nextInt();
+        System.out.println(x + " " + y);
+    }
+}
