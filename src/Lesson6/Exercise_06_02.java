26334088298e29a1e6802450e5045db4016caf1d
Your Name <rakzar041@gmail.com>
2024-09-06 17:45:33 +0300

    Урок 6. 2 Завдання.

---
+++ src/Lesson6/Exercise_06_02.java
@@ -0,0 +1,19 @@
+package Lesson6;
+
+import java.util.Scanner;
+
+public class Exercise_06_02  {
+    public static void main(String[] args) {
+        //2) Створити клас коло з полями, радіус, та константа PI. Додати методи обчислення площі, периметру.
+        System.out.println("Вважай що коло створено");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Програма сама обрахує площу за формулою (area = P * r2;)");
+        System.out.print("Введіть радіус, : ");
+        int radius = sc.nextInt();
+        double P = 3.14;
+        int r2 = radius * radius;
+        double area = P * r2;
+        System.out.println("Площа кола становить : " + area);
+
+    }
+}
