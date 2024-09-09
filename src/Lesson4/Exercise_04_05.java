eb47d90d07ea4e07f30888e30f9dfa39d22a5e81
Your Name <rakzar041@gmail.com>
2024-09-09 15:38:54 +0300

    Поправив 8 рядок my_array змінено на myArray.

--- src/Lesson4/Exercise_04_05.java
+++ src/Lesson4/Exercise_04_05.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 public class Exercise_04_05 {
     public static void main(String[] args) {
         //5) Напишіть програму на Java для видалення певного елемента з масиву.
-        int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        int[] myArray = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};//myArray
 
-        System.out.println("Оригінальний масив : " + Arrays.toString(my_array));
+        System.out.println("Оригінальний масив : " + Arrays.toString(myArray));
         int removeIndex = 1;
 
-        for (int i = removeIndex; i < my_array.length - 1; i++) {
-            my_array[i] = my_array[i + 1];
+        for (int i = removeIndex; i < myArray.length - 1; i++) {
+            myArray[i] = myArray[i + 1];
         }
 
-        System.out.println("Після видалення другого елемента: " + Arrays.toString(my_array));
+        System.out.println("Після видалення другого елемента: " + Arrays.toString(myArray));
     }
 }
