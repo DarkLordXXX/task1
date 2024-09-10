cec3a0b95bbe3ff33728c412b8222817f697210e
Your Name <rakzar041@gmail.com>
2024-09-10 10:16:30 +0300

    3.6. тепер без зайвого пробілу.

--- src/Lesson3/Homework06.java
+++ src/Lesson3/Homework06.java
@@ -7,7 +7,6 @@ public class Homework06 {
     public static void main(String[] args) {
         //2 урок 6 завдання. Напишіть програму для знаходження факторіалу будь-якого числа, введеного з клавіатури.
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Введіть число для обчислення факторіалу: ");
         int number = scanner.nextInt();
         BigInteger factorial = BigInteger.ONE;
