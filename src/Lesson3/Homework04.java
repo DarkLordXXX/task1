2df23ffaf97202143f73454a1a11a30ae06706c8
Your Name <rakzar041@gmail.com>
2024-09-10 10:14:05 +0300

    2.4. Добре без коментаря.

--- src/Lesson3/Homework04.java
+++ src/Lesson3/Homework04.java
@@ -5,7 +5,7 @@ public class Homework04 {
         //2 урок 4 завдання. Напишіть програму для обчислення суми перших 10 натуральних чисел.
         int  sum = 0;
         for (int i = 1; i <= 10; ++i) {
-            sum += i;// sum = sum + i;
+            sum += i;
         }
         System.out.println("Sum of First 10 Natural Numbers is = " + sum);
     }
