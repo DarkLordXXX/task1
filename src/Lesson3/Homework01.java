54de21eb0c9c4c19d11178eadd27c5d3f30ac7f9
Your Name <rakzar041@gmail.com>
2024-09-10 10:02:46 +0300

    2.1. тепер все правильно закинуло.

--- src/Lesson3/Homework01.java
+++ src/Lesson3/Homework01.java
@@ -2,7 +2,7 @@ package Lesson3;
 public class Homework01 {
     public static void main(String[] args) {
         //2 урок 1 завдання. Напишіть програму на Java для друку суми додавання, множення, віднімання, ділення та
-        //залишку двох чисел.
+        //залишку двох чисел..
         int a1 = 101;
         int a2 = 10;
         int addition = a1 + a2;
