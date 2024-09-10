1ba235ff2d5f95c8240f0b52bb425145dd1910f5
Your Name <rakzar041@gmail.com>
2024-09-10 09:03:49 +0300

    4.6.2 Змінив max_num на maxNum і min_num на minNum.і user_num на lengthOfTheArray.

--- src/Lesson4/Exercise_04_06_02.java
+++ src/Lesson4/Exercise_04_06_02.java
@@ -4,35 +4,35 @@ import java.util.Scanner;
 
 public class Exercise_04_06_02 {
     public static void main(String[] args) {
-        //6) Напишіть програму на Java для знаходження максимального та мінімального
+        //4.6) Напишіть програму на Java для знаходження максимального та мінімального
         //значення масиву.
         System.out.println("Введіть довжину масиву : ");
         Scanner scanner = new Scanner(System.in);
-        int user_num = scanner.nextInt();
+        int lengthOfTheArray = scanner.nextInt();
 
-        short[] nums = new short[user_num];
-        for (int i = 0; i < user_num; i++) {//for (short num : nums)
+        short[] nums = new short[lengthOfTheArray];
+        for (int i = 0; i < lengthOfTheArray; i++) {//for (short num : nums)
             System.out.println("Введіть елемент " + (i + 1) + " :");
             nums[i] = scanner.nextShort();
         }
 
-        short max_num = nums[0];
+        short maxNum = nums[0];
         for (short num : nums) {
-            if (num > max_num)
-                max_num = num;
+            if (num > maxNum)
+                maxNum = num;
         }
-        for (int i = 0; i < user_num; i++) {
-            if (nums[i] > max_num)
-                max_num = nums[i];
+        for (int i = 0; i < lengthOfTheArray; i++) {
+            if (nums[i] > maxNum)
+                maxNum = nums[i];
         }
-        System.out.println("Максимальний елемент :" + max_num);
+        System.out.println("Максимальний елемент :" + maxNum);
 
-        short min_num = nums[0];
+        short minNum = nums[0];
         for (short num : nums) {
-            if (num < min_num)
-                min_num = num;
+            if (num < minNum)
+                minNum = num;
         }
-        System.out.println("Мінімальний елемент :" + min_num);
+        System.out.println("Мінімальний елемент :" + minNum);
 
         for (short num : nums) {
              System.out.println(num);
