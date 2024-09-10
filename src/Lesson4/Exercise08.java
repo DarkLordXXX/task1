ec9cab99cb7b7ae46704c5961c1a4e2f8975719f
Your Name <rakzar041@gmail.com>
2024-09-10 10:47:27 +0300

    4.10. попраивив 16 рядок Set<Integer> commonElements = new HashSet<>(); і добавив ще одну умову.21-22 if.

--- src/Lesson4/Exercise08.java
+++ src/Lesson4/Exercise08.java
@@ -1,20 +1,27 @@
 package Lesson4;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Exercise08 {
     public static void main(String[] args) {
         //8) Напишіть програму на Java для пошуку спільних елементів між двома масивами цілих чисел
-        int[] array1 = {1, 2, 5, 5, 8, 9, 7, 10};
-        int[] array2 = {1, 0, 6, 15, 6, 4, 7, 0};
+        int[] array1 = {1, 2, 3, 3, 3, 5, 5, 8, 9, 7, 10};
+        int[] array2 = {1, 0, 3, 3, 3, 6, 15, 6, 4, 7, 0};
 
         System.out.println("Array1 : " + Arrays.toString(array1));
         System.out.println("Array2 : " + Arrays.toString(array2));
 
+        Set<Integer> commonElements = new HashSet<>();
+
         for (int i = 0; i < array1.length; i++) {
             for (int j = 0; j < array2.length; j++) {
                 if (array1[i] == (array2[j])) {
-                    System.out.println("Спільні елементи : " + (array1[i]));
+                    if (!commonElements.contains(array1[i])) {
+                        commonElements.add(array1[i]);
+                        System.out.println("Спільний елемент: " + array1[i]);
+                    }
                 }
             }
         }
