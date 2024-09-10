40dda215e04e374f3144765b30aed2b265e00f14
Your Name <rakzar041@gmail.com>
2024-09-10 08:50:28 +0300

    4.2. Добавив сканер для закриття при від'ємному числі.

--- src/Lesson4/Exercise02.java
+++ src/Lesson4/Exercise02.java
@@ -2,15 +2,16 @@ package Lesson4;
 
 import java.util.Scanner;
 
-public class Exercise_04_02 {
+public class Exercise02 {
     public static void main(String[] args) {
-        // ) Напишіть програму, яка знаходить суму кожного числа від 1 до num. Число завжди
+        // 4.2) Напишіть програму, яка знаходить суму кожного числа від 1 до num. Число завжди
         // буде додатним цілим числом, більшим за 0...
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введіть ціле число більше за 0: ");
         int num = scanner.nextInt();
         if (num <= 0) {
             System.out.println("Введене число має бути більше за 0.");
+            scanner.close();
         }
         if (num > 0) {
             int sum = 0;
@@ -18,7 +19,6 @@ public class Exercise_04_02 {
                 sum += i;
             }
             System.out.println("Сума чисел від 1 до " + num + " дорівнює " + sum);
-
             scanner.close();
         }
     }
