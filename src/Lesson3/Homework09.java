76dda0e9399cd63708ac187e73274dabec681401
Your Name <rakzar041@gmail.com>
2024-09-10 08:36:54 +0300

    3.9.Тепер без сканера і друкує тільки числа армстронга.

--- src/Lesson3/Homework09.java
+++ src/Lesson3/Homework09.java
@@ -1,6 +1,5 @@
 package Lesson3;
 
-import java.util.Scanner;
 
 public class Homework09 {
     public static void main(String[] args) {
@@ -8,24 +7,14 @@ public class Homework09 {
         цифри числа дорівнює самому числу, то це число називається числом Армстронга.
                 Наприклад, 153 = ( 1 * 1 * 1 ) + ( 5 * 5 * 5 ) + ( 3 * 3 * 3 )
         */
-        System.out.println("Введіть  число від 1-500 : ");
-        Scanner scanner = new Scanner(System.in);
-        int i = scanner.nextInt();//
-        int a = i / 100;
-        int b = i / 10 % 10;
-        int c = i % 10;
-        double kub = ((a * a * a) + (b * b * b) + (c * c * c));
-        if (kub == i && i <= 500) {
-            System.out.println(a + " a");
-            System.out.println(b + " a");
-            System.out.println(c + " a");
-            System.out.println("Це число Армстронга " + i + " == " + kub);
-        }
-        if (kub != i) {
-            System.out.println(a + " a");
-            System.out.println(b + " a");
-            System.out.println(c + " a");
-            System.out.println("Це число не є числом Армстронга : " + i + " != " + kub);
+        for (int i = 0; i <= 500; i++) {
+            int a = i / 100;
+            int b = i / 10 % 10;
+            int c = i % 10;
+            double numberOfArmstrong = ((a * a * a) + (b * b * b) + (c * c * c));
+            if (numberOfArmstrong == i) {
+                System.out.println("Це число Армстронга " + i + " == " + numberOfArmstrong);
+            }
         }
     }
 }
