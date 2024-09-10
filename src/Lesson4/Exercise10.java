5eff0cba96d2a4cd303b6381c30dad9233e5b185
Your Name <rakzar041@gmail.com>
2024-09-10 21:37:06 +0300

    4.10 Добавив пробіли.

--- src/Lesson4/Exercise10.java
+++ src/Lesson4/Exercise10.java
@@ -5,13 +5,12 @@ import java.util.Arrays;
 public class Exercise10 {
     public static void main(String[] args) {
         //4.10) Напишіть програму на Java для перевірки рівності двох масивів. (двомірних)
-        int[]array1 = { 30, 25, 40 };
-        int[]array2 = { 30, 25, 40 };
+        int[] array1 = {30, 25, 40};
+        int[] array2 = {30, 25, 40};
 
         if (Arrays.equals(array1, array2)) {
             System.out.println("Два масиви рівні");
-        }
-        else {
+        } else {
             System.out.println("Два масиви не рівні");
         }
     }
