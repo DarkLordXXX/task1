344ff7784907b5ffe75139e133f00692ebb98887
Your Name <rakzar041@gmail.com>
2024-09-10 09:35:35 +0300

    2.5. оголосив digit в середині циклу, number оголосив нижче, якщо опустити суму вибиває помилку.

--- src/Lesson3/Homework05.java
+++ src/Lesson3/Homework05.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Homework05 {
     public static void main(String[] args) {
         //2 урок 5 завдання. Напишіть програму для обчислення суми цифр цілого числа.
+        int sum = 0;
 
-        int number, digit, sum;
-        sum = 0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Введіть число: ");
-        number = sc.nextInt();
+        int number = sc.nextInt();
         while (number > 0) {
-            digit = number % 10;
+            int digit = number % 10;
             sum = sum + digit;
             number = number / 10;
         }
         System.out.println("Сума : " + sum);
+
     }
 }
 
