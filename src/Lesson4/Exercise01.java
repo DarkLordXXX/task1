f4e992b3e2f27cc23d785c5c9c95da692b76b54a
Your Name <rakzar041@gmail.com>
2024-09-12 22:44:43 +0300

    4.1 Та да ...

--- src/Lesson4/Exercise01.java
+++ src/Lesson4/Exercise01.java
@@ -1,10 +1,34 @@
 package Lesson4;
 
+import java.util.Scanner;
+
 public class Exercise01 {
     public static void main(String[] args) {
         //4.1) Створити функцію, яка може перетворити рядок на число.
-        String myString = "1234";
-        int stringToInt = Integer.parseInt(myString);
-        System.out.println(stringToInt);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть  цифри більші за 0: ");
+        String myString1  = scanner.nextLine();
+        System.out.print("Введіть  букви : ");
+        String myString2 = scanner.nextLine();
+        System.out.print("Введіть  пустий рядок : ");
+        String myString3 = scanner.nextLine();
+
+        System.out.println("Перетворення чисел введених з консолі  у вигляді рядка : " + stringToInt(myString1, 0));  // Виведе 123
+        System.out.println("Перетворення 'abc': " + stringToInt(myString2, 0));  // Виведе 0
+        System.out.println("Перетворення порожнього рядка: " + stringToInt(myString3, 0));  // Виведе 0
+    }
+    public static int stringToInt(String str, int defaultValue) {
+        if (str == null || str.isEmpty()) {
+            return defaultValue;
+        }
+
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
     }
 }
+//21 рядок Якщо рядок порожній або null, повертаємо значення за замовчуванням
+//26 Спроба перетворення рядка на число
+//28 Якщо рядок не можна перетворити, повертаємо значення за замовчуванням
